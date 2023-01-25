c00b020d01681e29c821ef054a2ce88ead0fa2dc
Darrin Barker <darrin.barker@flatironschool.com>
2023-01-25 12:33:54 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2);
+//returnFirstTwoDrivers();
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2)
+//returnLastTwoDrivers();
+const selectingDrivers = [];
+selectingDrivers[0] = returnFirstTwoDrivers;
+selectingDrivers[1] = returnLastTwoDrivers;
+
+//const createFareMultiplier = 4 => createFareMultiplier
+//createFareMultiplier()
+function createFareMultiplier (mult){
+    return function(fare){
+        return fare * mult
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, callback) => callback(drivers)
+   
